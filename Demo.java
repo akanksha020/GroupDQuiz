a0e6d99aa5323430bf9095898f0e9c5548562b91
akanksha020 <117468967+akanksha020@users.noreply.github.com>
2022-11-05 13:07:34 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,12 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		String s ="Characters";
+		for(int i=0;i<s.length();i++) {
+			System.out.print(s.charAt(i)+" ");
+		}
+	}
+
+}
